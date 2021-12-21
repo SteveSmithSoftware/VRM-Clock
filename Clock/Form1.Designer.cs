53f483ef00f70044cb05f10027d4b7fdcea15db5
Steve Smith <steve@stevesmith.software>
2021-12-21 19:47:35 +0100

    First release

---
+++ Clock/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Clock
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 60000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+			this.notifyIcon1.BalloonTipText = "Clock";
+			this.notifyIcon1.BalloonTipTitle = "Clock";
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "Clock";
+			this.notifyIcon1.Visible = true;
+			this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+			this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Consolas", 38F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(70, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(924, 70);
+			this.label1.TabIndex = 0;
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Consolas", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(70, 81);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(914, 83);
+			this.label2.TabIndex = 1;
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// label3
+			// 
+			this.label3.Font = new System.Drawing.Font("Consolas", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(1)));
+			this.label3.Location = new System.Drawing.Point(70, 164);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(914, 34);
+			this.label3.TabIndex = 2;
+			this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Window;
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.ClientSize = new System.Drawing.Size(1024, 270);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+			this.Text = "DateTime.Now()";
+			//this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ Clock/Form1.cs
@@ -0,0 +1,337 @@
+﻿/*
+ * Copyright SteveSmith.Software 2021. All Rights Reserverd.
+ * 
+ * Supplied under the MIT License
+ */
+
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Globalization;
+using Microsoft.Win32;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Configuration;
+
+namespace Clock
+{
+	public partial class Form1 : Form
+	{
+		bool dateset = false;
+
+		HttpClient httpClient = null;
+		HttpResponseMessage response = null;
+		Uri baseAddress;
+		string user;
+		string pass;
+		string responseData = null;
+		dynamic resp;
+		bool hasConnection=false;
+		string id = string.Empty;
+		float lastSoc = 0f;
+		string socTrend = "";
+		int itemCnt = 0;
+
+		string language;
+		Color textColor = Color.Goldenrod;
+
+		enum types
+		{
+			soc,
+			acIn,
+			acOut,
+			genSet,
+			pv,
+			COUNT
+		}
+
+		bool[] items = new bool[(int)types.COUNT];
+
+		public Form1()
+		{
+			InitializeComponent();
+			ShowInTaskbar = false;
+			this.BackColor = Color.Magenta;
+			label1.BackColor = Color.Magenta;
+			label2.BackColor = Color.Magenta;
+			label3.BackColor = Color.Magenta;
+			this.TransparencyKey = Color.Magenta;
+			notifyIcon1.Visible = true;
+			SystemEvents.PowerModeChanged += OnPowerChange;
+
+			int x = getConfigInt("positionX");
+			int y = getConfigInt("positionY");
+			Location = new Point(x, y);
+
+			string color = getConfigString("colour").ToLower();
+
+			string[] colors = Enum.GetNames(typeof(KnownColor));
+			int[] colVals = (int[])Enum.GetValues(typeof(KnownColor));
+			for (int i=0;i<colors.Length;i++)
+			{
+				if (color == colors[i].ToLower())
+				{
+					KnownColor kn = (KnownColor)colVals[i];
+					textColor = Color.FromKnownColor(kn);
+					break;
+				}
+			}
+
+			label1.ForeColor = textColor;
+			label2.ForeColor = textColor;
+			label3.ForeColor = textColor;
+
+			string url = "https://vrmapi.victronenergy.com/v2/";
+			baseAddress = new Uri(url);
+			string username = getConfigString("username"); 
+			string password = getConfigString("password");
+
+			if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+			{
+				label3.Text = "No Username or Password";
+			}
+			user = @"{ ""username"":""" + username + @""",";
+			pass = @" ""password"":""" + password + @"""}";
+
+			for (int i=0;i<(int)types.COUNT;i++)
+			{
+				items[i] = getConfigBool(((types)i).ToString());
+				if (items[i]) itemCnt++;
+			}
+
+			language = getConfigString("lang");
+			if (string.IsNullOrEmpty(language)) language = "en-US";
+
+			timer1_Tick(null, null);
+		}
+
+		bool TryGetConnection()
+		{
+			try
+			{
+				httpClient = new HttpClient();
+				httpClient.BaseAddress = baseAddress;
+				System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+				System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls12;
+
+				HttpContent content = new StringContent(user + pass, Encoding.UTF8, "application/json");
+
+				response = httpClient.PostAsync("auth/login", content).Result;
+				responseData = response.Content.ReadAsStringAsync().Result;
+				resp = JsonConvert.DeserializeObject<object>(responseData);
+				id = resp["idUser"];
+				string token = resp["token"];
+
+				httpClient.DefaultRequestHeaders.TryAddWithoutValidation("X-Authorization", "Bearer " + token);
+				return true;
+			}
+			catch 
+			{
+				label3.Text = "No connection available";
+			}
+			if (httpClient != null) httpClient.Dispose();
+			httpClient = null;
+			return false;
+		}
+
+		void timer1_Tick(object sender, EventArgs e)
+		{
+			label2.Text = DateTime.Now.ToShortTimeString();
+			label3.Text = GetData();
+			if (DateTime.Now.Hour == 0 && DateTime.Now.Minute == 0) dateset = false;
+			if (!dateset)
+			{
+				label1.Text = DateTime.Now.ToString("D", CultureInfo.CreateSpecificCulture(language));
+				dateset = true;
+			}
+			int miliseconds = 60000 - ((DateTime.Now.Second * 1000) + DateTime.Now.Millisecond);
+			timer1.Interval = miliseconds;
+		}
+
+		void notifyIcon1_DoubleClick(object sender, EventArgs e)
+		{
+			notifyIcon1.Visible = false;
+			SystemEvents.PowerModeChanged -= OnPowerChange;
+			if (hasConnection) httpClient.Dispose();
+			Close();
+		}
+
+		void notifyIcon1_Click(object sender, EventArgs e)
+		{
+			Visible = !Visible;
+		}
+
+		void OnPowerChange(object s, PowerModeChangedEventArgs e)
+		{
+			switch (e.Mode)
+			{
+				case PowerModes.Resume:
+					dateset = false;
+					timer1_Tick(null, null);
+					break;
+				case PowerModes.Suspend:
+					httpClient.Dispose();
+					httpClient = null;
+					hasConnection = false;
+					break;
+			}
+		}
+
+		string GetData()
+		{
+			try
+			{
+				if (!hasConnection)
+				{
+					hasConnection = TryGetConnection();
+				}
+
+				if (hasConnection)
+				{
+					response = httpClient.GetAsync("users/" + id + "/installations?extended=1").Result;
+					responseData = response.Content.ReadAsStringAsync().Result;
+					resp = JsonConvert.DeserializeObject<object>(responseData);
+					dynamic records = resp["records"];
+					dynamic record = records[0];
+					string siteId = record["idSite"];
+					dynamic extended = record["extended"];
+
+					string[] sa = new string[(int)types.COUNT];
+					for (int i = 0; i < (int)types.COUNT; i++)
+					{
+						sa[i] = string.Empty;
+					}
+
+					int cnt = 0;
+					bool finished = false;
+
+					for (int i = 0; i < extended.Count; i++)
+					{
+						dynamic data = extended[i];
+
+						string code = data["code"];
+						string value = data["formattedValue"];
+
+						switch (code)
+						{
+							case "bs":
+								cnt++;
+								if (!items[(int)types.soc]) break;
+								float soc = data["rawValue"];
+								if (lastSoc == 0f) lastSoc = soc;
+								if (soc > lastSoc) socTrend = "\U00002191";
+								else if (soc < lastSoc) socTrend = "\U00002193";
+								lastSoc = soc;
+								sa[(int)types.soc] = "SOC " + socTrend + value;
+								if (soc < 15f)
+								{
+									label3.ForeColor = Color.Red;
+								}
+								else
+								{
+									if (soc > 98)
+									{
+										label3.ForeColor = Color.PowderBlue;
+									}
+									else
+									{
+										label3.ForeColor = textColor;
+									}
+								}
+								if (cnt == itemCnt) finished = true;
+								break;
+							case "consumption":
+								cnt++;
+								if (!items[(int)types.acOut]) break;
+								if (data["rawValue"] != "0") sa[(int)types.acOut] = "AcOut " + value;
+								if (cnt == itemCnt) finished = true;
+								break;
+							case "ac_in":
+								cnt++;
+								if (!items[(int)types.acIn]) break;
+								if (data["rawValue"] != "0") sa[(int)types.acOut] = "AcIn " + value;
+								if (cnt == itemCnt) finished = true;
+								break;
+							case "solar_yield":
+								cnt++;
+								if (!items[(int)types.pv]) break;
+								if (data["rawValue"] != "0") sa[(int)types.pv] = "PV " + value;
+								if (cnt == itemCnt) finished = true;
+								break;
+							case "generator":
+								cnt++;
+								if (!items[(int)types.genSet]) break;
+								if (data["rawValue"] == null) break;
+								if (data["rawValue"] != "0") sa[(int)types.genSet] = "Genset " + value;
+								if (cnt == itemCnt) finished = true;
+								break;
+						}
+						if (finished) break;
+					}
+
+					string s = string.Join(" ", sa);
+					return s;
+				}
+			}
+			catch 
+			{
+			}
+			return "No Data available";
+
+		}
+
+		internal string getConfigString(string token)
+		{
+			try
+			{
+				string text = ConfigurationManager.AppSettings[token];
+				return text;
+			}
+			catch { }
+			return string.Empty;
+		}
+
+		internal bool getConfigBool(string token)
+		{
+			try
+			{
+				string text = ConfigurationManager.AppSettings[token];
+				bool n;
+				bool ok = Boolean.TryParse(text, out n);
+				if (ok)
+				{
+					return n;
+				}
+				else
+				{
+					Console.WriteLine("Invalid value in AppSettings for " + token);
+					return false;
+				}
+			}
+			catch { }
+			return false;
+		}
+
+		internal int getConfigInt(string token)
+		{
+			try
+			{
+				string text = ConfigurationManager.AppSettings[token];
+				int n;
+				bool ok = int.TryParse(text, out n);
+				if (ok)
+				{
+					return n;
+				}
+				else
+				{
+					Console.WriteLine("Invalid value in AppSettings for " + token);
+					return 0;
+				}
+			}
+			catch { }
+			return -1;
+		}
+	}
+}
